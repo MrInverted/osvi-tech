a41b5ea8ce5746fb061438c460a59d23282e58d0
MrInverted <akson.dyuk@gmail.com>
2024-03-14 18:20:29 +0200

    popups

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,13 @@
 (function () {
   try {
 
+    const userInfo = {
+      name: "",
+      tel: "",
+      course: "",
+      question: ""
+    }
+
     const addActiveClass = (item, activeClass, disabledClass) => {
       item.classList.add(activeClass);
       item.classList.remove(disabledClass);
@@ -53,9 +60,24 @@
           for (let item of chooserNodeList) {
             removeActiveClass(item, "opened", "closed");
           }
+
+          userInfo.course = badge.textContent;
         }
       })
     }
+
+    const closePopup = (popupBlock, popupContent, popupCloseButton) => {
+      popupBlock.onclick = (e) => {
+        if (!e.composedPath().includes(popupContent)) {
+          popupBlock.classList.remove("opened");
+        }
+      }
+
+      popupCloseButton.onclick = () => {
+        popupBlock.classList.remove("opened");
+      }
+    }
+
 
 
 
@@ -99,6 +121,97 @@
     switchTabs(potentialTabs, potentialChooser);
     openAndCloseChooser(potentialChooser);
     setMainTitle(potentialTitle, potentialBadge, potentialChooser);
+
+    // --------
+
+    const popupTabs = document.querySelectorAll(".popup .chooser__tab");
+    const popupChooser = document.querySelectorAll(".popup .chooser__select");
+    const popupTitle = document.querySelectorAll(".popup .chooser__title");
+    const popupBadge = document.querySelectorAll(".popup chooser-course");
+
+    switchTabs(popupTabs, popupChooser);
+    openAndCloseChooser(popupChooser);
+    setMainTitle(popupTitle, popupBadge, popupChooser);
+
+    const popup = document.querySelector(".popup");
+    const popupContent = document.querySelector(".popup .popup__content");
+    const popupClose = document.querySelector(".popup .popup__close");
+
+    const popupTriggers = document.querySelectorAll(".popup-trigger");
+
+    closePopup(popup, popupContent, popupClose);
+
+    popupTriggers.forEach(item => {
+      item.onclick = () => {
+        popup.classList.add("opened");
+
+        if (!userInfo.course) return;
+
+        for (let title of popupTitle) {
+          title.textContent = userInfo.course;
+        }
+      }
+    })
+
+    // --------
+
+    const popupQuestion = document.querySelector(".popup-question");
+    const popupQuestionContent = document.querySelector(".popup-question__content");
+    const popupQuestionCloseCross = document.querySelector(".popup-question__close");
+
+    const popupQuestionTrigger = document.querySelectorAll(".popup-question-trigger");
+
+    popupQuestionTrigger.forEach(item => {
+      item.onclick = () => {
+        popupQuestion.classList.add("opened");
+      }
+    })
+
+    closePopup(popupQuestion, popupQuestionContent, popupQuestionCloseCross);
+
+    // --------
+
+    const afterForm = document.querySelector(".after-form");
+    const afterFormContent = document.querySelector(".after-form__content");
+    const afterFormCloseCross = document.querySelector(".after-form__close");
+    const afterFormCloseButton = document.querySelector(".after-form button");
+
+    closePopup(afterForm, afterFormContent, afterFormCloseCross);
+    closePopup(afterForm, afterFormContent, afterFormCloseButton);
+
+    // --------
+
+    for (let form of document.forms) {
+      form.onsubmit = (e) => {
+        e.preventDefault();
+
+        if (e.target?.name?.value) userInfo.name = e.target.name.value;
+        if (e.target?.tel?.value) userInfo.tel = e.target.tel.value;
+        if (e.target?.question?.value) userInfo.question = e.target.question.value;
+
+        const formData = new FormData()
+        formData.append("name", userInfo.name);
+        formData.append("tel", userInfo.tel);
+        formData.append("course", userInfo.course);
+        formData.append("question", userInfo.question);
+
+        for (let item of formData.entries()) {
+          console.log(item)
+        }
+
+        fetch(e.target.action, {
+          method: "POST",
+          body: formData
+        }).then(s => console.log("sendind form..."))
+          .catch(e => console.log(e))
+
+        popup.classList.remove("opened");
+        popupQuestion.classList.remove("opened");
+        afterForm.classList.add("opened");
+
+        return false;
+      }
+    }
 
   } catch (error) {
     console.warn("Switchers' catch")
